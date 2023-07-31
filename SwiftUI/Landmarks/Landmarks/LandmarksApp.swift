71afd733cb00241c7144493855a841e10e6c84d6
jeehge <taerg89@gmail.com>
2023-07-31 23:25:26 +0900

    Create Project Landmarks

---
+++ SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by JH on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
